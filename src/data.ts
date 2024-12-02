7aed250b7b2726683b9f517c71d2011ae9863394
Hiriyan Mohammed <hireemoh@gmail.com>
2024-12-02 11:41:15 +0300

    data updted

--- src/data.ts
+++ src/data.ts
@@ -1,192 +1,127 @@
-// data.ts
+// // data.ts
+
+// export const articles = [
+//     {
+//       id: 1,
+//       title: "Mastering Tailwind CSS",
+//       description: "A guide to using Tailwind CSS for rapid UI development.",
+//       image: "/photo3.jpg",
+//       category: "Design",
+//       color: "bg-purple-500 text-blue-800",
+//       author: {
+//         name: "Gammada show",
+//         avatar: "http://surl.li/rvbfmo",
+//       },
+//       date: "5 days ago",
+//       link: "#",
+//     },
+//     {
+//       id: 2,
+//       title: "Introduction to Next.js",
+//       description: "Here are the Happy Guests of Todays Show You will hear many wonderful things from student and Walloo Senet Gizachew and Pro Gemechis Buba tonight We think the educated generation can learn a lot from both Etv and our YouTube channel Risaa Media",
+//       image: "/photo2.jpg",
+//       category: "JavaScript",
+//       color: "bg-green-100 text-green-800",
+//       author: {
+//         name: "Gammada show",
+//         avatar: "http://surl.li/rvbfmo",
+//       },
+//       date: "10 days ago",
+//       link: "#",
+//     },
+//     {
+//       id: 3,
+//       title: "React State Management",
+//       description: "Manage complex state efficiently using modern React patterns.",
+//       image: "/photo1.jpg",
+//       category: "React",
+//       color: "bg-blue-100 text-blue-800",
+//       author: {
+//         name: "Gammada show",
+//         avatar: "http://surl.li/rvbfmo",
+//       },
+//       date: "7 days ago",
+//       link: "#",
+//     },
+//     {
+//       id: 4,
+//       title: "Full-Stack Development Guide",
+//       description: "Combine frontend and backend for scalable applications.",
+//       image: "/Gammada.jpg",
+//       category: "Full-Stack",
+//       color: "bg-red-100 text-red-800",
+//       author: {
+//         name: "Gammada show",
+//         avatar: "http://surl.li/rvbfmo",
+//       },
+//       date: "2 days ago",
+//       link: "#",
+//       isFeatured: true, // Mark one article as featured
+//     },
+//   ];
+
+
+
+
+
+
+
 
 export const articles = [
-    {
-      id: 1,
-      title: "Mastering Tailwind CSS",
-      description: "A guide to using Tailwind CSS for rapid UI development.",
-      image: "/photo3.jpg",
-      category: "Design",
-      color: "bg-purple-500 text-blue-800",
-      author: {
-        name: "Gammada show",
-        avatar: "https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/jese-leos.png",
-      },
-      date: "5 days ago",
-      link: "#",
+  {
+    id: 1,
+    title: "Seenaa_Fiqiruu",
+    description: "Our guest today on Gammadaa Show is a talented and wonderful girl, #Seenaa_Fiqiruu🙏📌. The story of the songs of many of our artists is going to be heard again in a wonderful way with her kilole voice. We’ll be back at 3:15pm on Etv and our YouTube channel, and you’ll love it even more. Our program sponsor: Cooperative Bank of Orissa, Ethio Telecom, Mijuu Coffee.",
+    image: "/photo3.jpg",
+    category: "talented",
+    color: "bg-purple-500 text-blue-800",
+    author: {
+      name: "Gammada show",
+      avatar: "http://surl.li/rvbfmo",
     },
-    {
-      id: 2,
-      title: "Introduction to Next.js",
-      description: "Learn the basics of building web apps with Next.js.",
-      image: "/photo2.jpg",
-      category: "JavaScript",
-      color: "bg-green-100 text-green-800",
-      author: {
-        name: "Gammada show",
-        avatar: "https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/jese-leos.png",
-      },
-      date: "10 days ago",
-      link: "#",
+    date: "5 days ago",
+    link: "#",
+  },
+  {
+    id: 2,
+    title: "Senet_Gizachew",
+    description: "Here is the Guest you were predicting yesterday has come true. We called the talented and Common student #Senet_Gizachew and Pro #Gemechis_Desta_Buba and wished them success. There are many #surprises in this program and you will benefit from many educational programs. Thank you for letting us know in various ways that Gammadaa Show is your chosen program🙏🥊",
+    image: "/photo2.jpg",
+    category: "success",
+    color: "bg-green-100 text-green-800",
+    author: {
+      name: "Gammada show",
+      avatar: "http://surl.li/rvbfmo",
     },
-    {
-      id: 3,
-      title: "React State Management",
-      description: "Manage complex state efficiently using modern React patterns.",
-      image: "/photo1.jpg",
-      category: "React",
-      color: "bg-blue-100 text-blue-800",
-      author: {
-        name: "Gammada show",
-        avatar: "https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/jese-leos.png",
-      },
-      date: "7 days ago",
-      link: "#",
+    date: "10 days ago",
+    link: "#",
+  },
+  {
+    id: 3,
+    title: "Elias Yirdaw",
+    description: "Here’s today’s Happy Show Guest. We have brought you Elias Yirdaw, the young leader of FARIS Technology, a strong young man who has done great things in the field of technology and can be an example for this generation. Encourage your kids to watch today and we talked a lot about their favorite subject #AI and more. Stay tuned to Etv and our YouTube channel Risaa Media at 3:15pm. Our event sponsor: Cooperative Bank of Orissa, Ethio Telecom, Mijuu Coffee.",
+    image: "/faris.jpg",
+    category: "Technology",
+    color: "bg-blue-100 text-blue-800",
+    author: {
+      name: "Gammada show",
+      avatar: "http://surl.li/rvbfmo",
     },
-    {
-      id: 4,
-      title: "Full-Stack Development Guide",
-      description: "Combine frontend and backend for scalable applications.",
-      image: "/Gammada.jpg",
-      category: "Full-Stack",
-      color: "bg-red-100 text-red-800",
-      author: {
-        name: "Gammada show",
-        avatar: "https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/jese-leos.png",
-      },
-      date: "2 days ago",
-      link: "#",
-      isFeatured: true, // Mark one article as featured
+    date: "7 days ago",
+    link: "#",
+  },
+  {
+    id: 4,
+    title: "Introducing Gammada Show's Afan Oromo Podcast",
+    description: "Join us for an exciting episode of Gammada Show, where we dive into the world of Afan Oromo podcasts. This episode features interesting discussions, and great insights for the future.",
+    image: "/Gammada.jpg", 
+    category: "Podcast",
+    date: "1 day ago",
+    author: {
+      name: "Gammada Show",
+      avatar: "http://surl.li/rvbfmo",
     },
-  ];
-
-
-export const articlespopular = [
-    {
-      id: 1,
-      title: "Mastering Tailwind CSS",
-      description: "A guide to using Tailwind CSS for rapid UI development.",
-      image: "/photo9.jpg",
-      category: "Design",
-      color: "bg-purple-500 text-blue-800",
-      author: {
-        name: "Gammada show",
-        avatar: "https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/jese-leos.png",
-      },
-      date: "5 days ago",
-      link: "#",
-    },
-    {
-      id: 2,
-      title: "Introduction to Next.js",
-      description: "Learn the basics of building web apps with Next.js.",
-      image: "/photo8.jpg",
-      category: "JavaScript",
-      color: "bg-green-100 text-green-800",
-      author: {
-        name: "Gammada show",
-        avatar: "https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/jese-leos.png",
-      },
-      date: "10 days ago",
-      link: "#",
-    },
-    {
-      id: 3,
-      title: "React State Management",
-      description: "Manage complex state efficiently using modern React patterns.",
-      image: "/photo7.jpg",
-      category: "React",
-      color: "bg-blue-100 text-blue-800",
-      author: {
-        name: "Gammada show",
-        avatar: "https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/jese-leos.png",
-      },
-      date: "7 days ago",
-      link: "#",
-    },
-    {
-      id: 4,
-      title: "Full-Stack Development Guide",
-      description: "Combine frontend and backend for scalable applications.",
-      image: "/Gammada.jpg",
-      category: "Full-Stack",
-      color: "bg-red-100 text-red-800",
-      author: {
-        name: "Gammada show",
-        avatar: "https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/jese-leos.png",
-      },
-      date: "2 days ago",
-      link: "#",
-    },
-    {
-        id: 5,
-        title: "Introduction to Next.js",
-        description: "Learn the basics of building web apps with Next.js.",
-        image: "/photo6.jpg",
-        category: "JavaScript",
-        color: "bg-green-100 text-green-800",
-        author: {
-          name: "Gammada show",
-          avatar: "https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/jese-leos.png",
-        },
-        date: "10 days ago",
-        link: "#",
-      },
-      {
-        id: 6,
-        title: "React State Management",
-        description: "Manage complex state efficiently using modern React patterns.",
-        image: "/photo5.jpg",
-        category: "React",
-        color: "bg-blue-100 text-blue-800",
-        author: {
-          name: "Gammada show",
-          avatar: "https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/jese-leos.png",
-        },
-        date: "7 days ago",
-        link: "#",
-      },
-      {
-        id: 7,
-        title: "Mastering Tailwind CSS",
-        description: "A guide to using Tailwind CSS for rapid UI development.",
-        image: "/photo4.jpg",
-        category: "Design",
-        color: "bg-purple-500 text-blue-800",
-        author: {
-          name: "Gammada show",
-          avatar: "https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/jese-leos.png",
-        },
-        date: "5 days ago",
-        link: "#",
-      },
-      {
-        id: 8,
-        title: "Introduction to Next.js",
-        description: "Learn the basics of building web apps with Next.js.",
-        image: "/photo3.jpg",
-        category: "JavaScript",
-        color: "bg-green-100 text-green-800",
-        author: {
-          name: "Gammada show",
-          avatar: "https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/jese-leos.png",
-        },
-        date: "10 days ago",
-        link: "#",
-      },
-      {
-        id: 9,
-        title: "Introduction to Next.js",
-        description: "Learn the basics of building web apps with Next.js.",
-        image: "/photo2.jpg",
-        category: "JavaScript",
-        color: "bg-green-100 text-green-800",
-        author: {
-          name: "Gammada show",
-          avatar: "https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/jese-leos.png",
-        },
-        date: "10 days ago",
-        link: "#",
-      },
-  ];
-  
+    link: "#", 
+    isFeatured: true,
+  },
+];
